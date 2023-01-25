8a55b804224008971369dd169642496ba24e689b
AlenaMilkova <azarovahelena@yandex.ru>
2023-01-25 22:42:23 +0300

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] str;
+            Console.WriteLine("Введите массив строк через запятую");
+            string line = Console.ReadLine();
+            str = line.Split(',');
+            var result = new string[str.Length];
+            int size = 0;
+            foreach (var col in str)
+            {
+                if (col.Length <= 3)
+                {
+                    result[size] = col;
+                    size++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, size));
+            
